d8b56688f0bb21ccfefc73dfc1a9b6eb6d5a5bad
Xayanium <2909870181@qq.com>
2023-11-17 17:58:16 +0800

    create structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">YOJ</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PROBLEMSET</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GROUPS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTESTS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">RATING</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">DEVELOPER</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">HELP</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Register</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Problemset.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+
+<script>
+import ContentBase from '../components/ContentBase.vue';
+
+export default {
+    name: 'Problemset',
+    components: {
+        ContentBase,
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
